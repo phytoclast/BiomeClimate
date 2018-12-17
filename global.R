a0c9f71b7c0ff3933123b3b58fa9c08529764211
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2018-12-17 18:02:56 -0500

    Needed to set encoding
    Accented characters were not interpreted correctly on the Shiny server, even though they appear correctly in Rstudio.

--- global.R
+++ global.R
@@ -1,7 +1,8 @@
 library(shiny)
 Biomeclimate <- readRDS(file='data/Biomeclimate.RDS')
+Biomeclimate$ECO_NAME<- as.character(Biomeclimate$ECO_NAME) #needed to be able to control encoding
+Encoding(Biomeclimate$ECO_NAME) <- 'latin1' #needed to display correctly on server
 b1990 <- Biomeclimate[Biomeclimate$Norm == 1990,]
-
 b1990$PP <- apply(b1990[,c('p01','p02','p03','p04','p05','p06','p07','p08','p09','p10','p11','p12')], MARGIN = 1, FUN = 'sum')
 b1990$TT <- apply(b1990[,c('t01','t02','t03','t04','t05','t06','t07','t08','t09','t10','t11','t12')], MARGIN = 1, FUN = 'mean')
 b1990 <- b1990[,c('Latitude', 'Longitude','Elevation', 'PP', 'TT')]
